7a7f037dd75c34caad2e631499a4e0348309f8df
Mohit <themohitkumarsony@gmail.com>
2026-05-26 10:45:42 +0530

    initial commit

---
+++ basic-react-app/src/Button.jsx
@@ -0,0 +1,12 @@
+function handleClick(event) {
+  console.log("Hello");
+  console.log(event);
+}
+
+export default function Button() {
+  return (
+    <div>
+      <button onClick={handleClick}>Click me!</button>
+    </div>
+  );
+}
