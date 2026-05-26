4cac48acb4bfe447a646339ffefef3e4f88c2aed
Mohit <themohitkumarsony@gmail.com>
2026-05-26 19:54:45 +0530

    new commit

--- react-state-class2/src/App.jsx
+++ react-state-class2/src/App.jsx
@@ -1,17 +1,14 @@
-
-import './App.css'
+import "./App.css";
 import LudoBoard from "./LudoBoard";
 import TodoList from "./TodoList";
 import Lottery from "./Lottery";
 
 function App() {
-  
-
   return (
     <>
-      <Lottery/>
+      <Lottery n={3} winningSum={15}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
